82c532e496fa4c2202a7729d0d0b50911d92c8fb
Abhishek Prasad <devcula@outlook.com>
2020-09-23 16:17:15 +0530

    Linked vector icons for drawer

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 
@@ -31,7 +33,7 @@ const navHeaderStyles = {
   },
 };
 
-function HomeStackScreen() {
+function HomeStackScreen({navigation}) {
   return (
     <HomeStack.Navigator screenOptions={navHeaderStyles}>
       <HomeStack.Screen
@@ -39,13 +41,21 @@ function HomeStackScreen() {
         component={HomeScreen}
         options={{
           title: 'Home Page',
+          headerLeft: () => (
+            <Icon.Button
+              name="ios-menu"
+              size={25}
+              backgroundColor="#000000"
+              onPress={() => navigation.openDrawer()}
+            />
+          ),
         }}
       />
     </HomeStack.Navigator>
   );
 }
 
-function DetailsStackScreen() {
+function DetailsStackScreen({navigation}) {
   return (
     <DetailsStack.Navigator screenOptions={navHeaderStyles}>
       <DetailsStack.Screen
@@ -53,6 +63,14 @@ function DetailsStackScreen() {
         component={DetailsScreen}
         options={{
           title: 'Details Screen',
+          headerLeft: () => (
+            <Icon.Button
+              name="ios-menu"
+              size={25}
+              backgroundColor="#000000"
+              onPress={() => navigation.openDrawer()}
+            />
+          ),
         }}
       />
     </DetailsStack.Navigator>
